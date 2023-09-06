606fb85f40f081350d522a55f84bedf6da351018
masudcse9 github <masudcse9@gmail.com>
2023-09-06 19:20:41 +0600

    change config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  distDir: "build",
+  poweredByHeader: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
